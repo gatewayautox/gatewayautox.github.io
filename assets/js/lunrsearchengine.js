65489dc3b9ce1367671ef18d77894c7d81e39c8d
ChrisHammond <ChrisHammond@users.noreply.github.com>
2020-12-02 20:04:01 +0000

    deploy: 6c4036b27032bdca665b6b85bf66ea0a5aa4e383

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -11,106 +11,86 @@ var documents = [{
     "body": "Welcome to the internet home of the Gateway Autocross Association, an organization, established (again) in 2015 to bring you autocross events in the St. Louis area. We aren’t here to replace other autocross events, simply to add additional options to competitors that want a different kind of autocross experience, an experienced focused on having fun, and building up the level of competition in and around the St. Louis area. About Us: Gateway Autocross Association was originally formed in the 1970’s. In 2015 it was reborn to provide additional autocross options in the St. Louis Area. Gateway Autocross is presented by NASA-X Central Region. NASA-X Central Region is staffed by Dave, Beth and Samantha Whitworth, Janice Rick, Chris Hammond. Be sure to follow us on Twitter, Facebook and sign up for our newsletter to stay up to date with all the happenings. "
     }, {
     "id": 2,
-    "url": "/carsetup",
-    "title": "GatewayAutox.com - Car Setup",
-    "body": "350z Setup and Modifications List for STUWe’ll try to keep this list updated with current modifications, and a list of past modifications as well as we build and compete our 350z in the SCCA STU autocross class. Vehicle  Year: 2003 Make: Nissan Model: 350z Package: Touring Mileage: 149,000Front Suspension  Shocks: TrueChoice Phase 4 coilovers     (Removed) ISC threaded-body front shocks. Single-adjustable damping at top of shock, Independently preload-adjustable and height-adjustable. *    Front Springs: 650lb Hyperco Springs     (Removed) ISC, 14k rate (784#) *    Upper mounts: OEM     (Removed) ISC pillowball, aluminum *    Upper control arms: Kinetix, modified by Level Motorsports to optimize camber adjustment * Sway Bar: Hotchkis 3-way adjustable, urethane bushings *Rear Suspension  Shocks: Koni Sport (off the shelf)     (Removed) ISC, single-adjustable damping, threaded body for length adjustment. *    Shock mounts: ISC NOT-pillowball, aluminum * Rear Springs: 2. 5” id Hyperco, 600# rate * Rear Spring Perches: Megan Racing threaded collar adjustable * Rear Sway Bar: OEM Rear Camber Arms: Kinetix RacingEngine/Exhaust/Drivetrain The car came with an intake spacer, this will be removed before the Dixie Tour in Georgia, March 2016.  Intake: Jim Wolf cold air induction, shielded * Exhaust: Invidia Gemini, quad tips (added by P/O) * Battery: Deka 16# (?) * Differential: Tomei 1. 5 way locking unit, 85% lock * Intake Spacer: Unknown brand, not class legal, (removed March 2016). *Wheels/Tires  Wheels: XXR 531 * Tires: 275/285-35R18 Bridgestone RE-71R *InteriorThe interior is the Frost interior, originally with the leather,heated,electric seats. The Bose stereo components are mostly removed (AMP, sub, microphone).  Head unit: Pioneer Bluetooth XM-capable double-din touchscreen head unit w/iPod connection * Shift knob: Tomei composite * Shift boot: Alcantara one from eBay * Glass Tint: medium (approx 35%) *   Seats: NRG FRP 300 (added February 2016)        Anything with an asterisk came on the car when I purchased it in November 2015   "
-    }, {
-    "id": 3,
     "url": "/categories",
     "title": "Categories",
     "body": ""
     }, {
-    "id": 4,
+    "id": 3,
     "url": "/",
     "title": "Home",
     "body": "      Featured:                                                                                                                                                                                                             Looking for another autocross                              :               autocross:                                                                       30 Apr 2020                                                                                                                      All Stories:                                                             Looking for another autocross              :       autocross:                               30 Apr 2020                                            "
     }, {
-    "id": 5,
+    "id": 4,
     "url": "/photos",
     "title": "GatewayAutox.com Photos",
-    "body": "Photos from Gateway Autocross Assocation Events &lt;h3&gt;See more photos and add your own in our Flickr group&lt;/h3&gt; "
+    "body": "Photos from Gateway Autocross Assocation Events See more photos and add your own in our Flickr group: "
     }, {
-    "id": 6,
-    "url": "/videos",
-    "title": "GatewayAutox.com Videos",
-    "body": "Videos from the 350z "
-    }, {
-    "id": 7,
+    "id": 5,
     "url": "/About",
     "title": "",
     "body": ""
     }, {
-    "id": 8,
-    "url": "/Car-Setup",
-    "title": "",
-    "body": ""
-    }, {
-    "id": 9,
+    "id": 6,
     "url": "/Videos/Video-Player",
     "title": "",
     "body": ""
     }, {
-    "id": 10,
+    "id": 7,
     "url": "/Home/catpageid/2",
     "title": "",
     "body": ""
     }, {
-    "id": 11,
+    "id": 8,
     "url": "/Home/catpageid/3",
     "title": "",
     "body": ""
     }, {
-    "id": 12,
+    "id": 9,
     "url": "/Home/catpageid/4",
     "title": "",
     "body": ""
     }, {
-    "id": 13,
+    "id": 10,
     "url": "/Home/catpageid/5",
     "title": "",
     "body": ""
     }, {
-    "id": 14,
+    "id": 11,
     "url": "/Home/catpageid/6",
     "title": "",
     "body": ""
     }, {
-    "id": 15,
+    "id": 12,
     "url": "/Home/catpageid/7",
     "title": "",
     "body": ""
     }, {
-    "id": 16,
+    "id": 13,
     "url": "/Home/catpageid/8",
     "title": "",
     "body": ""
     }, {
-    "id": 17,
+    "id": 14,
     "url": "/Home/catpageid/9",
     "title": "",
     "body": ""
     }, {
-    "id": 18,
+    "id": 15,
     "url": "/Photos",
     "title": "",
     "body": ""
     }, {
-    "id": 19,
-    "url": "/Videos",
-    "title": "",
-    "body": ""
-    }, {
-    "id": 20,
+    "id": 16,
     "url": "/redirects.json",
     "title": "",
-    "body": "{“/About”:”/about”,”/Car-Setup”:”/carsetup”,”/Videos/Video-Player”:”/”,”/Home/catpageid/2”:”/”,”/Home/catpageid/3”:”/”,”/Home/catpageid/4”:”/”,”/Home/catpageid/5”:”/”,”/Home/catpageid/6”:”/”,”/Home/catpageid/7”:”/”,”/Home/catpageid/8”:”/”,”/Home/catpageid/9”:”/”,”/Photos”:”/photos”,”/Videos”:”/videos”} "
+    "body": "{“/About”:”/about”,”/Videos/Video-Player”:”/”,”/Home/catpageid/2”:”/”,”/Home/catpageid/3”:”/”,”/Home/catpageid/4”:”/”,”/Home/catpageid/5”:”/”,”/Home/catpageid/6”:”/”,”/Home/catpageid/7”:”/”,”/Home/catpageid/8”:”/”,”/Home/catpageid/9”:”/”,”/Photos”:”/photos”} "
     }, {
-    "id": 21,
+    "id": 17,
     "url": "/robots.txt",
     "title": "",
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
-    "id": 22,
+    "id": 18,
     "url": "/looking-for-another-autocross",
     "title": "Looking for another autocross",
     "body": "2020/04/30 - autocross "
